3a0299cb3d38dc6cb283dd4a32b6af3b583fb2ba
yexiucai <yexiucai@cs.tsukuba.ac.jp>
2023-06-30 22:32:20 +0900

    save current change before db used

--- xnote/QuestionDetailView.swift
+++ xnote/QuestionDetailView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct QuestionDetailView: View {
     @State private var isExpanded: Bool = false
+    @State private var isAnswerVisible = false
+
     var text : String = "問題の詳詳細の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内の詳詳細内容詳細内内容詳細内容します。"
     var body: some View {
         ScrollView{
@@ -37,37 +39,81 @@ struct QuestionDetailView: View {
                     Image(systemName: "note.text")
                     Text("解答")
                         .font(.headline)
+                    Spacer()
+                    Image(systemName: "lightbulb")
+                        .onTapGesture {
+                            isAnswerVisible.toggle()
+                        }
+                        .padding(15)
+                    
+                }
 
-                }
-                ScrollView(.vertical) {
-                    Text(text)
-                        .font(.body)
-                        .padding()
-                        .background(Color.white)
-                }
-                Image("sugaku")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding()
                 
-                HStack{
-                    Image(systemName: "note.text")
-                    Text("備考")
-                        .font(.headline)
-
-                }
-                ScrollView(.vertical) {
-                    Text("memo for ")
-                        .font(.body)
+                if isAnswerVisible {
+                    
+                    ScrollView(.vertical) {
+                        Text(text)
+                            .font(.body)
+                            .padding()
+                            .background(Color.white)
+                    }
+                    Image("sugaku")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
                         .padding()
-                        .background(Color.white)
+                    
+                    HStack{
+                        Image(systemName: "note.text")
+                        Text("備考")
+                            .font(.headline)
+                        
+                    }
+                    
+                    ScrollView(.vertical) {
+                        Text("memo for ")
+                            .font(.body)
+                            .padding()
+                            .background(Color.white)
+                    }
                 }
+                    
+                HStack{
+                    Spacer()
+                    
+                    Button(action: {
+                        // 添加保存逻辑
+                        saveQuestion()
+                    }) {
+                        Text("保存")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: UIScreen.main.bounds.width / 2)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    Spacer()
+                }
+                .padding(30)
+
+                
             }
             .padding(10)
         }
         
     }
 }
+
+
+func saveQuestion() {
+    // 添加保存逻辑
+    // 在这里实现将问题保存的代码
+}
+
+
+
+
+
 
 struct QuestionDetailView_Previews: PreviewProvider {
     static var previews: some View {
